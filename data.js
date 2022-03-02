90d38b3b3f819f5f9f10350a2742d93635c8f46d
lai2-01 <lai2-01@itismeucci.com>
2022-03-02 13:10:03 +0100

    colegamento server

--- data.js
+++ data.js
@@ -1,51 +1,10 @@
-var dati = [
-  {
-    "id": 10001,
-    "birthDate": "1953-09-01",
-    "firstName": "Georgi",
-    "lastName": "Facello",
-    "gender": "M",
-    "hireDate": "1986-06-25",
-  },
-  {
-    "id": 10002,
-    "birthDate": "1964-06-01",
-    "firstName": "Bezalel",
-    "lastName": "Simmel",
-    "gender": "F",
-    "hireDate": "1985-11-20",
-  },
-  {
-    "id": 10003,
-    "birthDate": "1959-12-02",
-    "firstName": "Parto",
-    "lastName": "Bamford",
-    "gender": "M",
-    "hireDate": "1986-08-27T22:00:00.000+0000",
-  },
-  {
-    "id": 10004,
-    "birthDate": "1954-04-30",
-    "firstName": "Chirstian",
-    "lastName": "Koblick",
-    "gender": "M",
-    "hireDate": "1986-11-30",
-
-  },
-  {
-    "id": 10005,
-    "birthDate": "1955-01-20",
-    "firstName": "Kyoichi",
-    "lastName": "Maliniak",
-    "gender": "M",
-    "hireDate": "1989-09-11T22:00:00.000+0000",
-
-  }
-]
 
 var nexId = 10006;
 
 $(document).ready(function () {
+
+  // url iniziale
+  callServer("http://localhost:8080/employees");
   
 //elimina
   $("body").on("click", ".btn-delete", function () {
@@ -59,7 +18,7 @@ $(document).ready(function () {
     }
     displayTable();
   })
-  displayTable();
+  
 
 //aggiungi
   $("body").on("click", ".btn-add", function () {
@@ -76,6 +35,7 @@ $(document).ready(function () {
     dati.push(nuovo);
     displayTable();
     nexId++;
+    
 
     //chiusura dopo aggiungi
     var modal= $('#exampleModal');
@@ -104,9 +64,16 @@ $(document).ready(function () {
 
 });
 
+function callServer(url) {
+  $.get( url, function( response ) {
+    console.log(response);
+    displayTable(response["_embedded"]["employees"]);
+    //displayPagination(response["page"], response["_links"]);
+  });
 
+}
 
-function displayTable() {
+function displayTable(dati) {
   var r = '';
   $.each(dati, function (id, value) {
     r += '<tr>';
