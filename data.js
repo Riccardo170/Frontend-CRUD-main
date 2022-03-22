66d5ece50d6fc1bb80475cead8aa1c4eed0468d7
riccardo <riccagallo67@gmail.com>
2022-03-22 19:17:16 +0100

    da finire modifica

--- data.js
+++ data.js
@@ -1,43 +1,65 @@
-
+var url= 'http://localhost:8080/employees';
 var nexId = 10006;
 var next;
 var last;
 var prev;
 var first;
+var response=null;
+var id;
 
 $(document).ready(function () {
 
   // url iniziale
-  chiamataServer("http://localhost:8080/employees");
+  chiamataServer(url);
 //elimina
-  $("body").on("click", ".btn-delete", function () {
+  $("body").on("click", "#btn-delete", function () {
+    $(this).parents("tr").fadeOut("fast");
     var td = $(this).parent("td");
     var id = td.data("id");
-    for (var i = 0; i < dati.length; i++) {
-      if (dati[i].id == id) {
-        dati.splice(i, 1);
-        break;
-      }
-    }
-    displayTachiamataServerble();
+    $.ajax({
+      url: url+'/'+id,// url + id dell'utente selezionato
+      type: "delete", //si dice di vooler cancellare dal db
+      success: function(data){
+        displayTable(url+"?page="+dati['page']['number']+"&size=20");}//e se ha successo(significa che è stata cancellata) si da la url all display table
   })
+  });
   
 
 //aggiungi
-  $("body").on("click", ".btn-add", function () {
+  $("body").on("click", "#btn-add", function () {
     var firstName = $('#firstName').val();
     console.log(firstName);
     var lastname = $('#lastname').val();
     console.log(lastname);
+
     var nuovo = {
       "id": nexId,
       "firstName": firstName,
       "lastName": lastname,
       "gender": "M",
+
     }
-    dati.push(nuovo);
-    displayTable();
-    nexId++;
+    $.ajax({
+      type: "POST",
+      url: url,
+
+      data: JSON.stringify({
+          birthDate: "",
+          firstName: firstName,
+          gender: "M",
+          hireDate: "",
+          lastName: lastname,
+      }),
+
+      contentType: "application/json",
+      dataType: 'json',
+
+      success: function () {
+          var last = response["_links"]["last"]["href"];
+          console.log(last);
+          chiamataServer(last);
+      }
+  });
     
 
     //chiusura dopo aggiungi
@@ -48,37 +70,53 @@ $(document).ready(function () {
   });
 
   //modifica
-  $("body").on("click", ".btn-modifica", function () {
+  $(".modifica").click(function () {
+    id = $(this).parent().data("id");
 
-    var modal= $('#modalmodifica');
-    modal.modal("show");
-    var td = $(this).parent("td");
-    var id = td.data("id");
-    for (var i = 0; i < dati.length; i++) {
-      if (dati[i].id == id) {
-        $('#nome').val(dati[i].firstName);
-        $('#cognome').val(dati[i].lastName);
-        break;
-      }
+    for (var i = 0; i < data.length; i++) {
+        if (id == data[i].id) {
+            $("#nome-m").val(data[i].firstName);
+            $("#cognome-m").val(data[i].lastName);
+        }
     }
+});
+  $("body").on("click", "#btn-modifica", function () {
+      var nome = $("#nome").val();
+      var cognome = $("#cognome").val();
+      
+      console.log(id);
+
+      $.get( url+"/"+id, function(data) {
+          console.log(data);
+          data.firstName=nome;
+          data.lastName=cognome;
+          $.ajax({
+              type: "PUT", //si dice di vooler aggiornare dal db
+              url: url+'/'+id,// url + id dell'utente selezionato
+              data : JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                dataType: "JSON",
+                success: function(data){displayTable(url+"?page="+dati['page']['number']+"&size=20");}//e se ha successo(significa che è stata cancellata) si da la url all display table
+          })
+          })
+      
   });
-
-
-
-
 });
 
-function chiamataServer(url) {
+function chiamataServer(link) {
   $.ajax({
-    url: url,
-    success: function( response ) {
+    url: link,
+    success: function( responseData ) {
+      response=responseData;
       displayTable(response["_embedded"]["employees"]);
+      console.log(response)
       //displayPagination(response["page"], response["_links"]);
-      next = response['_links']['next']['href']
+      if(response["page"]["number"]!=response["page"]["totalPages"]-1){
+        next = response['_links']['next']['href']
+      }
       last=response['_links']['last']['href']
       prev=response['_links']['prev']['href']
       first=response['_links']['first']['href']
-      console.log(next);
       
     },
     dataType: 'json'
@@ -98,7 +136,7 @@ $('#last').click(function(){
 });
 
 $('#prev').click(function(){
-  console.log(prev);
+  console.log("prev:"+prev);
   chiamataServer(prev);
 });
 
@@ -114,8 +152,8 @@ function displayTable(dati) {
     r += '<td>' + value.id + '</td>';
     r += '<td>' + value.firstName + '</td>';
     r += '<td>' + value.lastName + '</td>';
-    r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-danger btn-delete">Elimina</button>' + '</td>';
-    r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-warning btn-modifica">Modifica</button>' + '</td>';
+    r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-danger" id="btn-delete">Elimina</button>' + '</td>';
+    r += '<td data-id=' + value.id + '> <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalmodifica" id="btn-modifica">Modifica</button>' + '</td>';
     r += '<tr>' + '</tr>';
   });
   $("tbody").html(r);
